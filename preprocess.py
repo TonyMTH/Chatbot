60dc840eb37896c98160613e3e58e383e2d714a6
Nwachukwu Anthony <nwachukwuanthony2015@gmail.com>
2021-12-10 02:31:10 +0100

    Include preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,12 @@
+import pickle
+from pathlib import Path
+
+import spacy
 import torch
 import json
+
+from torch.utils.data import Dataset, DataLoader
+from torchtext.vocab import FastText
 
 torch.manual_seed(0)
 
@@ -8,3 +15,103 @@ def load_data(path):
     with open(path, 'r') as f:
         intents = json.load(f)
     return intents
+
+
+def train_test_split(df, test=0.3):
+    idx = int(df.shape[0] * (1 - test))
+    return df.iloc[:idx, :], df.iloc[idx:, :]
+
+
+def preprocessing(sentence):
+    """
+    params sentence: a str containing the sentence we want to preprocess
+    return the tokens list
+    """
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(str(sentence))
+    tokens = [token.text.lower() for token in doc if not token.is_punct and not token.is_stop]
+    return tokens
+
+
+def token_encoder(token, vec):
+    if token == "<pad>":
+        return 1
+    else:
+        try:
+            return vec.stoi[token]
+        except:
+            return 0
+
+
+def encoder(tokens, vec):
+    return [token_encoder(token, vec) for token in tokens]
+
+
+def padding(list_of_indexes, max_seq_len, padding_index=1):
+    output = list_of_indexes + (max_seq_len - len(list_of_indexes)) * [padding_index]
+    return output[:max_seq_len]
+
+
+def load_fasttext(PIK):
+    if not Path(PIK).is_file():
+        vec = FastText()
+        vec.vectors[1] = -torch.ones(vec.vectors[1].shape[0])
+        vec.vectors[0] = torch.zeros(vec.vectors[0].shape[0])
+        with open(PIK, "wb") as f:
+            pickle.dump(vec, f)
+    with open(PIK, "rb") as f:
+        vec = pickle.load(f)
+    return vec
+
+
+def clean_load(intents, fasttext_PIK, PIK, max_seq_len):
+    if not Path(PIK).is_file():
+        clean(intents, fasttext_PIK, PIK, max_seq_len)
+    with open(PIK, "rb") as f:
+        data = pickle.load(f)
+    return data
+
+
+def clean(intents, fasttext_PIK, PIK, max_seq_len):
+    vec = load_fasttext(fasttext_PIK)
+    x, y = [], []
+
+    y_dic = {}
+    last_index = 0
+
+    for intent in intents['intents']:
+        if intent['tag'] not in y_dic.keys():
+            y_dic[intent['tag']] = last_index
+            last_index += 1
+        y_val = y_dic[intent['tag']]
+        for pattern in intent['patterns']:
+            y.append(y_val)
+            sequence = padding(encoder(preprocessing(pattern), vec), max_seq_len=max_seq_len)
+            x.append(sequence)
+
+    data = {'x': x, 'y': y, 'y_dic': y_dic}
+
+    print("Dumping file")
+    with open(PIK, "wb") as f:
+        pickle.dump(data, f)
+    print("Done Dumping")
+
+
+class ChatData(Dataset):
+    def __init__(self, x, y):
+        self.n_samples = len(x)
+        self.x_data = x
+        self.y_data = y
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return self.n_samples
+
+
+def data_loader(x, y, batch_size, num_workers=0):
+    dataset = ChatData(x, y)
+    return DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+
--- train.py
+++ train.py
@@ -1,17 +1,49 @@
 import torch
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import RandomOverSampler
+
 import preprocess as pr
 from parameters import *
+import model as md
+
 torch.manual_seed(0)
 
 # Define Processor
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("1.\t"+str(device.type).capitalize()+" detected")
+print("1.\t" + str(device.type).capitalize() + " detected")
 
 # Fetch Data
-df_train = pr.load_data(data_file)
+intents = pr.load_data(data_file)
 
+# Preprocess Data
+print('2.\tProcessing data.......')
+data = pr.clean_load(intents, fasttext_PIK, PIK, max_seq_len)
+X, y, y_dic = data['x'], data['y'], data['y_dic']
 
+# Split Data
+print('2.\tTrain/Test Split.......')
+train_X, test_X, y_train, y_test = train_test_split(X, y, test_size=.3, stratify=y, random_state=0)
 
+# Oversample Train Data
+print('3.\tOversampling Train Data.......')
+ros = RandomOverSampler(random_state=0)
+train_X, y_train = ros.fit_resample(train_X, y_train)
+
+# Data Loaders
+print('4.\tData Loaders.......')
+train_loader = pr.data_loader(train_X, y_train, batch_size)
+test_loader = pr.data_loader(test_X, y_test, batch_size)
+
+# Define Model
+model = md.Model(max_seq_len, hidden_size, len(y_dic.keys()))
+model.to(device)
+print("2.\tModel defined and moved to " + str(device.__str__()))
+
+# Parameters
+optimizer = Optimizer(model.parameters())
+print("3.\tCriterion set as " + str(criterion.__str__()))
+print("4.\tOptimizer set as " + str(optimizer.__str__()))
 
 if __name__ == '__main__':
-    print(df_train)
+    # print(y_test,y_train)
+    pass
