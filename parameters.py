54a4f76ff5f90c3d36b4910a9c66677bc04a3d1f
Nwachukwu Anthony <nwachukwuanthony2015@gmail.com>
2021-12-10 02:31:26 +0100

    Include model

--- parameters.py
+++ parameters.py
@@ -1,2 +1,21 @@
+import torch
+from torch import nn, optim
 
 data_file = 'data/intents.json'
+fasttext_PIK = 'data/fasttext.dat'
+max_seq_len = 10
+PIK = "data/data_pickle.dat"
+batch_size = 5
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+lr = 0.01
+criterion = nn.CrossEntropyLoss()
+Optimizer = lambda x: optim.Adam(x, lr=lr)
+
+epochs = 10
+printing_gap = 1
+model_path = 'data/best_model.pt'
+saved_model_device = torch.device("cpu")
+
+hidden_size = 8
